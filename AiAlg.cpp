65b86d83d7cc4de5695aa027d77ae2ddd425fa55
C00241212 <c00241212@itcarlow.ie>
2022-01-13 15:20:41 +0000

    Fixed min max alterating

--- AiAlg.cpp
+++ AiAlg.cpp
@@ -2,22 +2,22 @@
 
 Move AiAlg::executeMove(sf::RenderWindow* t_window, std::array<Board*, 4> t_boards, Piece* t_piece, GameState& t_currentState)
 {
-    Move bestMove = getBestMove(t_currentState, t_boards, 0, Move());
+    Move bestMove = getBestMove(t_currentState, t_boards, t_piece, 0, Move());
     t_boards.at(bestMove.z)->getTile(bestMove.x, bestMove.y)->owner() = t_piece->type();
     t_piece->addPosition(PieceData{ (unsigned int)bestMove.x, (unsigned int)bestMove.y, bestMove.z });
     moves.clear();
     return bestMove;
 }
 
-Move AiAlg::getBestMove(GameState& t_currentState, std::array<Board*, 4> t_boards, int t_depth, Move t_move)
+Move AiAlg::getBestMove(GameState t_currentState, std::array<Board*, 4> t_boards, Piece* t_piece, int t_depth, Move t_move)
 {
     if (t_currentState == AI_WIN)
     {
-        return Move(10 - t_depth);
+        return Move(1000 - t_depth);
     }
     else if (t_currentState == PLAYER_WIN)
     {
-        return Move(t_depth - 10);
+        return Move(t_depth - 1000);
     }
 
     if (t_depth >= MAX_DEPTH)
@@ -47,12 +47,12 @@ Move AiAlg::getBestMove(GameState& t_currentState, std::array<Board*, 4> t_board
                     if (t_currentState == PLAYER_TURN)
                     {
                         t_boards.at(z)->getTile(x, y)->owner() = PieceCheck::PLAYER;
-                        move.score = getBestMove(t_currentState, t_boards, t_depth + 1, move).score;
+                        move.score = getBestMove(GameState::AI_TURN, t_boards, t_piece, t_depth + 1, move).score;
                     }
                     if (t_currentState == AI_TURN)
                     {
                         t_boards.at(z)->getTile(x, y)->owner() = PieceCheck::AI;
-                        move.score = getBestMove(t_currentState, t_boards, t_depth + 1, move).score;
+                        move.score = getBestMove(GameState::PLAYER_TURN, t_boards, t_piece, t_depth + 1, move).score;
                     }
 
                     moves.push_back(move);
--- AiAlg.h
+++ AiAlg.h
@@ -7,7 +7,7 @@ class Game;
 
 struct Move
 {
-	Move() : score{0} {};
+	Move() : score{ 0 } {};
 	Move(int t_score) : score{ t_score } {};
 
 	int score;
@@ -21,7 +21,7 @@ public:
 	Move executeMove(sf::RenderWindow* t_window, std::array<Board*, 4> t_boards, Piece* t_piece, GameState& t_currentState);
 
 private:
-	Move getBestMove(GameState& t_currentState, std::array<Board*, 4> t_boards, int t_depth, Move t_move);
+	Move getBestMove(GameState t_currentState, std::array<Board*, 4> t_boards, Piece* t_piece, int t_depth, Move t_move);
 	int const MAX_DEPTH = 2;
 	std::vector<Move> moves;
 };
